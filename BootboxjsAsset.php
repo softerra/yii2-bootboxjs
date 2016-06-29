2fa8fce2eb5376c4eca60141e870501a5e2a84ee
ran <ran.ran@liketry.com>
2016-06-29 16:07:55 +0800

    up

--- BootboxjsAsset.php
+++ BootboxjsAsset.php
@@ -8,6 +8,7 @@
 
 namespace huijiewei\bootboxjs;
 
+use Yii;
 use yii\web\AssetBundle;
 
 class BootboxjsAsset extends AssetBundle
@@ -17,4 +18,28 @@ class BootboxjsAsset extends AssetBundle
     public $js = [
         'js/bootbox.js',
     ];
+	
+	   /**
+     * Registers this asset bundle with a view.
+     * @param \yii\web\View $view the view to be registered with
+     * @return static the registered asset bundle instance
+     */
+    public static function registerWithOverride($view)
+    {
+        $assetbundle = self::register($view);
+		
+        Yii::$app->view->registerJs('
+            yii.confirm = function(message, ok, cancel) {
+                bootbox.confirm(message, function(result) {
+                    if (result) { !ok || ok(); } else { !cancel || cancel(); }
+                });
+            }
+        ');
+		
+        if(Yii::$app->language !== null && strlen(Yii::$app->language)>=2) {
+            Yii::$app->view->registerJs('bootbox.setDefaults({locale: "'.substr(Yii::$app->language,0,2).'"});');
+        }
+        
+        return $assetbundle;
+    }
 }
